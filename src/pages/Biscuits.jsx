9519754af594e0cc58a4b64ee3348e9eae7b0904
Mennad92 <boutaleb.mennad@gmail.com>
2024-07-20 13:40:28 +0200

    avancement

--- src/pages/Biscuits.jsx
+++ src/pages/Biscuits.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 import ProductList from '../components/Product/ProductList';
+import productService from "../services/productService";
 
 const Biscuits = () => {
+  const [products, setProducts] = useState([]);
+  const [isFetched, setIsFetched] = useState(false);
+
+  useEffect(() => {
+    !isFetched &&
+      productService.getProducts().then((json) => {
+        setProducts(json);
+        setIsFetched(true);
+      });
+  }, [isFetched]);
   return (
     <div>
       <h2>Biscuits</h2>
-      <ProductList category="biscuits" />
+      {products.length > 0 ? (
+        <ProductList category="biscuits" products={{ products }} />
+      ) : (
+        <p>Aucun produit disponible pour cette catégorie.</p>
+      )}
     </div>
   );
 };
