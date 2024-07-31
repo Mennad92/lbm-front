dd7539a4b982e50faa419566a2208da3c148ce6d
Mennad92 <boutaleb.mennad@gmail.com>
2024-08-01 01:22:38 +0200

    WIP implement profile update

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -3,11 +3,12 @@ import profileService from '../services/profileService';
 import AddressForm from '../components/order/AddressForm';
 import { useAuthStore } from '../stores/authStore';
 import { useNavigate } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 const Profile = () => {
   const navigate = useNavigate();
   const isLoggedIn = useAuthStore((state) => state.isLoggedIn());
-  const [profile, setProfile] = useState(null);
+  const [isUpdated, setIsUpdated] = useState(true);
   const [error, setError] = useState(null);
   const [profileForm, setProfileForm] = useState({
     firstName: '',
@@ -23,7 +24,13 @@ const Profile = () => {
       ...profileForm,
       [event.target.name]: event.target.value
     });
+    setIsUpdated(false);
   };
+
+  async function handleSubmit() {
+    const data = await profileService.updateProfile(profileForm);
+  };
+
 
   useEffect(() => {
     if (!isLoggedIn) {
@@ -35,7 +42,6 @@ const Profile = () => {
     const fetchProfile = async () => {
       try {
         const data = await profileService.getProfile();
-        setProfile(data);
         setProfileForm({
           firstName: data.first_name || '',
           lastName: data.last_name || '',
@@ -60,6 +66,16 @@ const Profile = () => {
         <div>
           <h2>Profil</h2>
           <AddressForm profileData={profileForm} handleChange={handleChange} />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+            disabled={isUpdated}
+            onClick={handleSubmit()}
+          >
+            Mettre à jour le profil
+          </Button>
            
         </div>
       ) : (
